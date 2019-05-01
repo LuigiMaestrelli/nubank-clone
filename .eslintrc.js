2cb3e48d17c0ebb599aab9d55f3790a623d49028
Luigi Maestrelli <lgmaestrelli@gmail.com>
2019-05-01 19:37:00 -0300

    Finalizado projeto de animações

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,33 +2,33 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-        'plugin:react-native/all',
+        'plugin:react-native/all'
     ],
     env: {
         'react-native/react-native': true,
-        jest: true,
+        jest: true
     },
     parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
             ecmaVersion: 8,
             experimentalObjectRestSpread: true,
-            jsx: true,
+            jsx: true
         },
-        sourceType: 'module',
+        sourceType: 'module'
     },
     settings: {
         react: {
             createClass: 'createReactClass',
             pragma: 'React',
             version: 'detect',
-            flowVersion: '0.93',
+            flowVersion: '0.93'
         },
         propWrapperFunctions: [
             'forbidExtraProps',
-            { property: 'freeze', object: 'Object' },
+            { property: 'freeze', object: 'Object' }
         ],
-        linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }],
+        linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }]
     },
     plugins: ['react', 'react-native'],
     rules: {
@@ -45,5 +45,6 @@ module.exports = {
         'react-native/no-inline-styles': ['warn'],
         'react-native/no-color-literals': ['off'],
         'react-native/sort-styles': ['off'],
-    },
+        'react-native/no-raw-text': 'off'
+    }
 };
